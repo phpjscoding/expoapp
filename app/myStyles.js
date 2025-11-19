9a7917a2fa6f76b13c38046becf63d7a99b257a7
phpjscoding <phpjscoder@gmail.com>
2025-11-19 11:58:17 +0100

    first commit

---
+++ app/myStyles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+ const MyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'brown',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+  },
+});
+
+export default MyStyles
